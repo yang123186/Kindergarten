2cb003e8f7bebdd0da0385e48902a681b06f6b55
Rdd7 <rdd7@rdd7.com>
2015-05-15 16:23:00 +0800

    Initial Commit

---
+++ KinderGarten_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KinderGarten_iOS
+//
+//  Created by Rdd7 on 5/15/15.
+//  Copyright (c) 2015 Rdd7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KinderGarten_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KinderGarten_iOS
+//
+//  Created by Rdd7 on 5/15/15.
+//  Copyright (c) 2015 Rdd7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
